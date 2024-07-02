c15cf6be58fd5ce714ec90b98c60b4b9fe60bf73
konst <konst@Petsoo>
2024-07-02 21:00:29 +0300

    Populate course.json and courseschema.json with a basic structure

--- public_html/Task1/course.js
+++ public_html/Task1/course.js
@@ -1,4 +1,4 @@
-/* 
+    /* 
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
  */
